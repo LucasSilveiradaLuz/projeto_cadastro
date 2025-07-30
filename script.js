9b6eb5ba3dbe843596df4f1cad269ab9e5dac6be
Lucas <luzlucas22022007@gmail.com>
2025-07-29 22:03:08 -0300

    Enviando arquivos do projeto

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
+const form = document.getElementById("form");
+const lista = document.getElementById("lista"); // Adicione uma <ul id="lista"></ul> no HTML para ver a mensagem
+//Adiciona um evento de submit ao formulário
+form.addEventListener("submit", cadastro);
 
-const paciente = document.querySelector(".form_grupo")
-const sexo = document.querySelector("radio-btn")
-const button = document.querySelector("submit")
-const quarto = document.getElementById("quarto")
+function cadastro(e) {
+  e.preventDefault();
+// Captura os valores dos campos do formulário e armazena no localStorage
+let nome = document.getElementById("nome").value;
+localStorage.setItem('nome',nome)
+let sobrenome = document.getElementById("sobrenome").value;
+ localStorage.setItem('sobrenome',sobrenome)
+let dataDeNascimento = document.getElementById("dataDeNascimento").value;
+localStorage.setItem('dataDeNascimento',dataDeNascimento)
+let quarto =document.getElementById("quarto").value;
+localStorage.setItem('quarto',quarto)
+ let responsavel = document.getElementById("responsavel").value;
+localStorage.setItem('responsavel',responsavel)
+ let genero = document.querySelector('input[name="genero"]:checked').value;
+localStorage.setItem('genero',genero)
+let especificacao = document.querySelector('input[name="especificacao"]:checked').value;
+localStorage.setItem('especificacao',especificacao)
+//Verifica se os campos estão preenchidos 
+ if (!nome ||!sobrenome ||!dataDeNascimento || !responsavel || !genero ||!especificacao) {
 
-button.addEventListener("click", cadastro);
-paciente.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    cadastro();
-  }
-});
+  alert("Por favor, preencha todos os campos corretamente.");
 
-function cadastro(){
- 
-const input = paciente 
-  if (paciente === "") {
-    alert("Por favor, preencha todos os campos.");
-    return;
-  }
-  if (quarto.length < 3) {
-    alert("O quarto deve ter pelo menos 3 dígitos.");
-    return;
-  }
-   else{
-      const enviado = document.createElement("li");
-    enviado.innerHTML = ""
-    input.appendChild(enviado);
-   }
-console.log(paciente)
- 
-  }
-    
-  
+  return;
+
+ }
+ // Mostra mensagem de concluído em alert
+ alert("Concluído");
+ // Limpa os campos do formulário
+ form.reset();
+ // Remove seleção manualmente dos radios (caso algum navegador não limpe)
+  document.querySelectorAll('input[type="radio"]').forEach(radio => radio.checked = false);
+}
+
+
 
 
 
